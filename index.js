daf961bc695d99d4bbb380055786dc3700835742
Scott Bender <scott@scottbender.net>
2018-01-21 17:13:41 -0500

    Change name to concentrate2

--- index.js
+++ index.js
@@ -4,30 +4,30 @@ var stream = require("stream"),
 const Int64LE = require('int64-buffer').Int64LE
 const Uint64LE = require('int64-buffer').Uint64LE
 
-var Concentrate = module.exports = function Concentrate(options) {
-  if (!(this instanceof Concentrate)) { return new Concentrate(options); }
+var Concentrate2 = module.exports = function Concentrate2(options) {
+  if (!(this instanceof Concentrate2)) { return new Concentrate2(options); }
 
   stream.Readable.call(this, options);
 
   this.jobs = [];
 };
-util.inherits(Concentrate, stream.Readable);
+util.inherits(Concentrate2, stream.Readable);
 
-Concentrate.prototype._read = function _read(n) {};
+Concentrate2.prototype._read = function _read(n) {};
 
-Concentrate.prototype.copy = function copy() {
-  var copy = new Concentrate();
+Concentrate2.prototype.copy = function copy() {
+  var copy = new Concentrate2();
   copy.jobs = this.jobs.slice(0);
   return copy;
 };
 
-Concentrate.prototype.reset = function reset() {
+Concentrate2.prototype.reset = function reset() {
   this.jobs.splice(0);
 
   return this;
 };
 
-Concentrate.prototype.result = function result() {
+Concentrate2.prototype.result = function result() {
   // optimisation for if there's only one job and it's a buffer - we don't need
   // to compile anything, we can just shove the buffer right on through. keep on
   // truckin'.
@@ -38,7 +38,7 @@ Concentrate.prototype.result = function result() {
   var buffer = new Buffer(this.jobs.reduce(function(i, v) { return i + v.length; }, 0));
 
   var offset = 0;
-  this.jobs.forEach(function(job) {
+  this.jobs.forEach(function(job, index) {
     var method = ["write", job.type].join("_");
 
     if (job.fields) {
@@ -51,47 +51,52 @@ Concentrate.prototype.result = function result() {
     }
     
     if (typeof this[method] === "function") {
-      offset += this[method](job, buffer, offset);
+      try {
+        offset += this[method](job, buffer, offset);
+      } catch ( err ) {
+        console.log(`unable to write job at index ${index} offset ${offset}`)
+        throw err
+      }
     }
   }.bind(this));
 
   return buffer;
 };
 
-Concentrate.prototype.flush = function flush(no_reset) {
+Concentrate2.prototype.flush = function flush(no_reset) {
   this.push(this.result());
   this.reset();
 
   return this;
 };
 
-Concentrate.prototype.end = function end() {
+Concentrate2.prototype.end = function end() {
   this.push(null);
 
   return this;
 };
 
-Concentrate.prototype.write_number = function write_number(job, buffer, offset) {
+Concentrate2.prototype.write_number = function write_number(job, buffer, offset) {
   buffer[job.method](job.data, offset);
   return job.length;
 };
 
-Concentrate.prototype.write_buffer = function write_buffer(job, buffer, offset) {
+Concentrate2.prototype.write_buffer = function write_buffer(job, buffer, offset) {
   job.data.copy(buffer, offset);
   return job.data.length;
 };
 
-Concentrate.prototype.buffer = function buffer(data) {
+Concentrate2.prototype.buffer = function buffer(data) {
   this.jobs.push({type: "buffer", data: data, length: data.length});
   return this;
 };
 
-Concentrate.prototype.string = function string(data, encoding) {
+Concentrate2.prototype.string = function string(data, encoding) {
   return this.buffer(new Buffer(data, encoding));
 };
 
 
-Concentrate.prototype.uint64 = function uint64(data) {
+Concentrate2.prototype.uint64 = function uint64(data) {
   /*
    const b = new Buffer(8)
    const MAX_UINT32 = 0xFFFFFFFF
@@ -104,7 +109,7 @@ Concentrate.prototype.uint64 = function uint64(data) {
   return this.buffer(new Uint64LE(data).toBuffer())
 };
 
-Concentrate.prototype.uint64 = function int64(data) {
+Concentrate2.prototype.int64 = function int64(data) {
   return this.buffer(new Int64LE(data).toBuffer())
 };
 
@@ -124,7 +129,7 @@ Concentrate.prototype.uint64 = function int64(data) {
           method = ["write", s.toUpperCase(), "Int", b, endiannes.toUpperCase()].join(""),
           length = b / 8;
 
-      Concentrate.prototype[type] = function(data) {
+      Concentrate2.prototype[type] = function(data) {
         this.jobs.push({
           type: "number",
           method: method,
@@ -144,7 +149,7 @@ Concentrate.prototype.uint64 = function int64(data) {
         method = ["write", t[0].replace(/^(.)/, function(e) { return e.toUpperCase(); }), e.toUpperCase()].join(""),
         length = t[1];
 
-    Concentrate.prototype[type] = function(data) {
+    Concentrate2.prototype[type] = function(data) {
       this.jobs.push({
         type: "number",
         method: method,
@@ -158,7 +163,7 @@ Concentrate.prototype.uint64 = function int64(data) {
 });
 
 
-Concentrate.prototype.tinyInt = function(value, bits) {
+Concentrate2.prototype.tinyInt = function(value, bits) {
   var tinyJob = makeLastJobTiny(this.jobs);
   tinyJob.fields.unshift({
     value: value,
